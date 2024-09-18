6ed446b6a15f406e65531b85f9c4e6942eec776c
JasonBernstein1 <jstatbernstein@gmail.com>
2024-09-17 22:33:34 -0600

    Refactor fetch_tle: update name, add arguments, simplify data extraction

--- scripts/fetch_tle.py
+++ scripts/fetch_tle.py
@@ -11,38 +11,19 @@ iss_norad_id = 25544
 file_path = "iss_tle.json"
 
 
-def fetch_iss_tle():
+def fetch_tle(url: str, norad_cat_id: int):
     # Send a GET request to the CelesTrak API
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
         # Loop through all satellites in the response
         for sat in data:
-            if sat["NORAD_CAT_ID"] == iss_norad_id:
-                # Extract the JSON data for the ISS
-                tle_data = {
-                    "OBJECT_NAME": sat["OBJECT_NAME"],
-                    "OBJECT_ID": sat["OBJECT_ID"],
-                    "EPOCH": sat["EPOCH"],
-                    "MEAN_MOTION": sat["MEAN_MOTION"],
-                    "ECCENTRICITY": sat["ECCENTRICITY"],
-                    "INCLINATION": sat["INCLINATION"],
-                    "RA_OF_ASC_NODE": sat["RA_OF_ASC_NODE"],
-                    "ARG_OF_PERICENTER": sat["ARG_OF_PERICENTER"],
-                    "MEAN_ANOMALY": sat["MEAN_ANOMALY"],
-                    "EPHEMERIS_TYPE": sat["EPHEMERIS_TYPE"],
-                    "CLASSIFICATION_TYPE": sat["CLASSIFICATION_TYPE"],
-                    "NORAD_CAT_ID": sat["NORAD_CAT_ID"],
-                    "ELEMENT_SET_NO": sat["ELEMENT_SET_NO"],
-                    "REV_AT_EPOCH": sat["REV_AT_EPOCH"],
-                    "BSTAR": sat["BSTAR"],
-                    "MEAN_MOTION_DOT": sat["MEAN_MOTION_DOT"],
-                    "MEAN_MOTION_DDOT": sat["MEAN_MOTION_DDOT"],
-                    "date_fetched": datetime.now(timezone.utc)
-                    .isoformat()
-                    .replace("+00:00", "")
-                    + "Z",
-                }
+            if sat["NORAD_CAT_ID"] == norad_cat_id:
+                # Extract the JSON data for the given ID
+                tle_data = sat
+                tle_data["date_fetched"] = (
+                    datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
+                )
                 return tle_data
     return None
 
@@ -68,7 +49,7 @@ def append_tle(tle_data):
 
 
 if __name__ == "__main__":
-    tle = fetch_iss_tle()
+    tle = fetch_tle(url, iss_norad_id)
     if tle:
         append_tle(tle)
         print(f"ISS TLE fetched and appended: {tle}")
